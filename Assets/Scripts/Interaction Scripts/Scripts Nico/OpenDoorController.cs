0d65cf5a4a11541dc54e5b97fdba0cb317d2fc19
NikoMyoji <nicolas.vlte@gmail.com>
2023-01-03 14:46:17 +0100

    moify OpenDoorController

--- Assets/Scripts/Interaction Scripts/Scripts Nico/OpenDoorController.cs
+++ Assets/Scripts/Interaction Scripts/Scripts Nico/OpenDoorController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class OpenDoorController : MonoBehaviour
 {
- 
+    private bool _isOpen = false;
 
     // Update is called once per frame
     void Update()
@@ -13,6 +13,15 @@ public class OpenDoorController : MonoBehaviour
     }
     public void OpenCloseDoor()
     {
-        transform.eulerAngles = new Vector3(0f, 90f, 90f);
+        if (_isOpen)
+        {
+            transform.eulerAngles = new Vector3(0f, -90f, 90f);
+            _isOpen = false;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0f, 90f, 90f);
+            _isOpen = true;
+        }
     }
 }
