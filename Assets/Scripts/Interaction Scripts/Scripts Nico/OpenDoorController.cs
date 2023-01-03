1d44767ff48183bb27a7aa9b43892f0447c19a88
NikoMyoji <nicolas.vlte@gmail.com>
2023-01-03 14:39:27 +0100

    Dooor Script creation

---
+++ Assets/Scripts/Interaction Scripts/Scripts Nico/OpenDoorController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorController : MonoBehaviour
+{
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public void OpenCLoseDoor()
+    {
+        transform.eulerAngles = new Vector3(0f, 90f, 90f);
+    }
+}
