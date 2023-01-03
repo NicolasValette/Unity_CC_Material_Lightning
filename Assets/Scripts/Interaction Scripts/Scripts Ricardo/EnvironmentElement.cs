a9547c65beca84485abfa61c62b81bda15e92c08
RicardoLuqueMartos <lmricmail@yahoo.fr>
2023-01-03 11:32:32 +0100

    environmentElement component created

---
+++ Assets/Scripts/Interaction Scripts/Scripts Ricardo/EnvironmentElement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static MaterialData;
+
+public class EnvironmentElement : MonoBehaviour
+{
+    public MaterialData.MaterialTypeEnum MaterialType = new MaterialData.MaterialTypeEnum();
+
+}
